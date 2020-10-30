ca1c342daa2a9af43b58f41834af359baf890a97
carlschr <cjcarlson136@gmail.com>
2020-10-30 18:54:17 -0500

    Add styling

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const renderSchedule = () => {
     //Creates schedule blocks for the hours of 9 am to 5 pm
     for (let i = 9; i < 18; i++) {
         //Creates Bootstrap row and column
-        let newRow = $(`<div class="row"></div>`);
+        let newRow = $(`<div class="row m-3"></div>`);
         let newCol = $(`<div class="col-12"></div>`);
         //Creates bootsrap input group, text, and button inline
         let newInputGroup = $(`<div class="input-group input-group-lg"></div>`);
         let newInputPrepend = $(`<div class="input-group-prepend"><h1 class="input-group-text bg-primary text-light">${i > 12 ? i - 12 + ' PM' : i + ' AM'}</h1></div>`);
         let newInput = $(`<input type="text" class="form-control" aria-label="hour${i - 8}" aria-describedby="hour${i - 8}">`);
-        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-outline-secondary" type="button" id="hour${i - 8}">+</button></div>`);
+        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-dark" type="button" id="hour${i - 8}">+</button></div>`);
 
         //Appends the elements in the proper structure
         eventHolder.append(newRow);
