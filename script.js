89fbd6c3d9877347f080c41cbeea4ca73c1e0b6c
carlschr <cjcarlson136@gmail.com>
2020-10-30 20:12:25 -0500

    Add additional styling

--- script.js
+++ script.js
@@ -20,11 +20,12 @@ const renderSchedule = () => {
         //Creates Bootstrap row and column
         let newRow = $(`<div class="row m-3"></div>`);
         let newCol = $(`<div class="col-12"></div>`);
+
         //Creates bootsrap input group, text, and button inline
         let newInputGroup = $(`<div class="input-group input-group-lg"></div>`);
         let newInputPrepend = $(`<div class="input-group-prepend"><h1 class="input-group-text bg-primary text-light">${i > 12 ? i - 12 + ' PM' : i + ' AM'}</h1></div>`);
         let newInput = $(`<input type="text" class="form-control" aria-label="hour${i - 8}" aria-describedby="hour${i - 8}">`);
-        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-dark" type="button" id="hour${i - 8}">+ -</button></div>`);
+        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-dark" type="button" id="hour${i - 8}">+ | -</button></div>`);
 
         //Sets the placeholder equal to the saved data for the hour
         //If there is no data then the placeholder is an empty string
