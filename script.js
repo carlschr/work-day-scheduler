9453eacde389ced9e7e73adf5fc26bcb7bcc1795
carlschr <cjcarlson136@gmail.com>
2020-10-30 19:34:17 -0500

    Add logic to interact with local storage

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const renderSchedule = () => {
     //Changes the currentDay text to the curent date
     $('#currentDay').text(date);
 
+    //Clear the html within container
+    eventHolder.html('');
+
     //Creates schedule blocks for the hours of 9 am to 5 pm
     for (let i = 9; i < 18; i++) {
         //Creates Bootstrap row and column
@@ -21,7 +24,20 @@ const renderSchedule = () => {
         let newInputGroup = $(`<div class="input-group input-group-lg"></div>`);
         let newInputPrepend = $(`<div class="input-group-prepend"><h1 class="input-group-text bg-primary text-light">${i > 12 ? i - 12 + ' PM' : i + ' AM'}</h1></div>`);
         let newInput = $(`<input type="text" class="form-control" aria-label="hour${i - 8}" aria-describedby="hour${i - 8}">`);
-        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-dark" type="button" id="hour${i - 8}">+</button></div>`);
+        let newInputAppend = $(`<div class="input-group-append"><button class="btn btn-dark" type="button" id="hour${i - 8}">+ -</button></div>`);
+
+        //Sets the placeholder equal to the saved data for the hour
+        //If there is no data then the placeholder is an empty string
+        newInput.attr('placeholder', `${localStorage.getItem('hour' + (i - 8)) ? localStorage.getItem('hour' + (i - 8)) : ''}`)
+
+        //Adds click event for the button that adds the corresponding input value to local storage
+        newInputAppend.on('click', (event) => {
+            let inputButton = $(event.currentTarget).find('button');
+            let inputBox = $(event.currentTarget).parent().find('input');
+            localStorage.setItem(inputButton.attr('id'), inputBox.val());
+            inputBox.attr('placeholder', `${inputBox.val()}`);
+            inputBox.val('');
+        });
 
         //Appends the elements in the proper structure
         eventHolder.append(newRow);
